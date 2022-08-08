cd47bde1e550b82954bcdb224cb46440796b0903
AFarence <83982643+AFarence@users.noreply.github.com>
2022-08-08 15:42:45 -0400

    chi_arch_map_success_push

--- MAPBOX_top_chi_archs_2017_and_later/map.js
+++ MAPBOX_top_chi_archs_2017_and_later/map.js
@@ -5,3 +5,51 @@ var map = new mapboxgl.Map({
     zoom: 8.5,
     center: [-87.7, 41.83]
 });
+
+map.on('load', function () {
+
+    let layers = map.getStyle().layers;
+    let firstSymbolId;
+    for (var i = 0; i < layers.length; i++) {
+        if (layers[i].type === 'symbol') {
+            firstSymbolId = layers[i].id;
+            break;
+        }
+    }
+    map.addLayer({
+        'id':'arch_data',
+        'type':'circle',
+        'source': {
+            'type': 'geojson',
+            'data':'data/chi_archs.geojson'
+        },
+        'paint': {
+            'circle-color': '#ff7f50',
+            'circle-stroke-color': '#4d4d4d',
+            'circle-stroke-width': 0.5,
+            'circle-radius': 5
+        }
+    }, firstSymbolId);
+});
+// Create the popup
+map.on('click', 'arch_data', function (e) {
+    let full_address = e.features[0].properties.full_address;
+    let name = e.features[0].properties.name;
+    let work_desc = e.features[0].properties.work_desc;
+    let year_start = e.features[0].properties.app_year_start;
+    new mapboxgl.Popup()
+        .setLngLat(e.lngLat)
+        .setHTML('<h4>' + full_address + '</h4>' 
+        + '<strong>Application year:</strong> ' + year_start + '<br>'
+        + '<strong>Architect:</strong> ' + name + '<br><br>'
+        + '<strong>Description:</strong> ' + work_desc)
+        .addTo(map);
+});
+// Change the cursor to a pointer when the mouse is over the turnstileData layer.
+map.on('mouseenter', 'arch_data', function () {
+    map.getCanvas().style.cursor = 'pointer';
+});
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'arch_data', function () {
+    map.getCanvas().style.cursor = '';
+});
