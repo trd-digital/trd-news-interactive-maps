0bb64c604b53bb2cf58de3840f941dfdaeece44a
AFarence <83982643+AFarence@users.noreply.github.com>
2022-08-08 13:09:11 -0400

    weekly condo sales, first week of august

---
+++ MAPBOX_top_chi_archs_2017_and_later/map.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoidHJkZGF0YSIsImEiOiJjbDZoMTNpNDYwMW1lM2NyeG0wdm80OWFzIn0.aOdBF7K4eHzAelsBIy52lQ';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/trddata/cl6h00632006t14o08g2ppqvj',
+    zoom: 8.5,
+    center: [-87.7, 41.83]
+});
