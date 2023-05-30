00c196ea6f721e33d344ed40723282bd4772ef37
AFarence <83982643+AFarence@users.noreply.github.com>
2023-05-30 17:56:35 -0400

    updated map

--- Agricultural_Properties/map.js
+++ Agricultural_Properties/map.js
@@ -11,48 +11,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     map.on('load', function () {
         // Add the geojson source and layer
-        map.addSource('MD_agri_data_source', {
+        map.addSource('SoFla_agri_data_source', {
             type: 'geojson',
-            data: 'data/MD_agri.geojson'
+            data: 'data/SoFla_agri.geojson'
         });
 
         map.addLayer({
-            id: 'MD_agri_data',
+            id: 'SoFla_agri_data',
             type: 'fill',
-            source: 'MD_agri_data_source',
-            paint: {
-                'fill-color': '#FF0000',
-                'fill-outline-color': '#00FFFF',
-                'fill-opacity': 0.7
-            }
-        });
-
-        map.addSource('PB_agri_data_source', {
-            type: 'geojson',
-            data: 'data/PB_agri.geojson'
-        });
-
-        map.addLayer({
-            id: 'PB_agri_data',
-            type: 'circle',
-            source: 'PB_agri_data_source',
-            paint: {
-              'circle-color': '#FF0000',
-              'circle-stroke-color': '#00FFFF',
-              'circle-stroke-width': 1,
-              'circle-opacity': 0.7
-            }
-        })
-
-        map.addSource('BC_agri_data_source', {
-            type: 'geojson',
-            data: 'data/BC_agri.geojson'
-        });
-
-        map.addLayer({
-            id: 'BC_agri_data',
-            type: 'fill',
-            source: 'BC_agri_data_source',
+            source: 'SoFla_agri_data_source',
             paint: {
                 'fill-color': '#FF0000',
                 'fill-outline-color': '#00FFFF',
@@ -114,88 +81,29 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 
-    // Change the cursor to a pointer when the mouse is over the MD_agri_data layer.
-    map.on('mouseenter', 'MD_agri_data', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-    map.on('mouseenter', 'PB_agri_data', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
-
-    map.on('mouseenter', 'BC_agri_data', function () {
+    // Change the cursor to a pointer when the mouse is over the SoFla_agri_data layer.
+    map.on('mouseenter', 'SoFla_agri_data', function () {
         map.getCanvas().style.cursor = 'pointer';
     });
 
     // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'MD_agri_data', function () {
-        map.getCanvas().style.cursor = '';
-    });
-
-    // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'PB_agri_data', function () {
-        map.getCanvas().style.cursor = '';
-    });
-
-    // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'BC_agri_data', function () {
+    map.on('mouseleave', 'SoFla_agri_data', function () {
         map.getCanvas().style.cursor = '';
     });
 
     // Create the popup
-    map.on('click', 'MD_agri_data', function (e) {
-        let address = e.features[0].properties.TRUE_SITE_ADDR;
-        let folio = e.features[0].properties.FOLIO;
-        let zone = e.features[0].properties.DOR_DESC;
-
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML('<h2>' + address + '</h2>'
-                + '<strong>Folio:</strong> ' + folio + '<br>'
-                + '<strong>Zone Description:</strong> ' + zone)
-            .addTo(map);
-    });
-
-    // Create the PB popup
-    map.on('click', 'PB_agri_data', function (e) {
-        let address = e.features[0].properties.full_address;
-        let folio = e.features[0].properties.PARCEL_NUMBER;
-        let zone = e.features[0].properties.PROPERTY_USE;
-
-        new mapboxgl.Popup()
-            .setLngLat(e.lngLat)
-            .setHTML('<h2>' + address + '</h2>'
-                + '<strong>Folio:</strong> ' + folio + '<br>'
-                + '<strong>Zone Description:</strong> ' + zone)
-            .addTo(map);
-    });
-
-    // Create the BC popup
-    map.on('click', 'BC_agri_data', function (e) {
+    map.on('click', 'SoFla_agri_data', function (e) {
         let address = e.features[0].properties.PHY_ADDR1;
+        let city = e.features[0].properties.PHY_CITY;
         let folio = e.features[0].properties.PARCEL_ID;
         let zone = e.features[0].properties.DOR_UC;
 
         new mapboxgl.Popup()
             .setLngLat(e.lngLat)
-            .setHTML('<h2>' + address + '</h2>'
+            .setHTML('<h2>' + address + ' ' + city + '</h2>'
                 + '<strong>Folio:</strong> ' + folio + '<br>'
                 + '<strong>Zone Description:</strong> ' + zone)
             .addTo(map);
     });
 
-    // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'MD_agri_data', function () {
-        map.getCanvas().style.cursor = '';
-    });
-
-    // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'PB_agri_data', function () {
-        map.getCanvas().style.cursor = '';
-    });
-
-    // Change it back to the default cursor when it leaves.
-    map.on('mouseleave', 'BC_agri_data', function () {
-        map.getCanvas().style.cursor = '';
-    });
 });
