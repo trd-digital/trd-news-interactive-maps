d42e1ca5e24dd8fbf93c813927f2dbec5496a684
AFarence <83982643+AFarence@users.noreply.github.com>
2023-07-31 18:25:25 -0400

    Update config.js

--- Boulevard_broken_deals/config.js
+++ Boulevard_broken_deals/config.js
@@ -9,47 +9,47 @@ let introDiv =
 let divChapter1 =
   "<h3>600 West Jackson</h3>" +
 //  "<p><strong>Status:</strong> Sold in Oct. 2022, for $10.8M ($92 psf)<br><strong>Current mortgage:</strong> $7.8M, Lake Forest Bank & Trust, Oct. 2022<br><strong>Landlord/Buyer:</strong> Farbman Group<br><strong>Seller:</strong> Stockbridge Capital Group<br><strong>Last sale price:</strong> $23.5M, 2017 ($200 psf)<br><strong>Last mortgage:</strong> $20.5M, Citizens Bank, Feb. 2018<br><strong>Year built:</strong> 1911</p>" +
-  "<canvas id='600_West_Jackson' height='400'></canvas>";
+  "<canvas id='600_West_Jackson' height='300'></canvas>";
 
 let divChapter2 =
   "<h3>300 South Riverside Plaza</h3>" +
   // "<p><strong>Status:</strong> Leasehold owner in default<br><strong>Leasehold owner:</strong> JV of David Werner and Joseph Mizrachi<br><strong>Current leasehold mortgage:</strong> $175M, Shinhan Investment Corp., 2017 (in default, per Morningstar)<br><strong>Ground lease debt:</strong> $167M, CMBS debt originated by Morgan Stanley<br><strong>Ground landlord:</strong> JV of Rubin Schron and David Lowenfeld<br><strong>Last sale price:</strong> $220M for land only, 2015<br><strong>Year built:</strong> 1987</p>" +
-  "<canvas id='300_South_Riverside_Plaza' height='400'></canvas>";
+  "<canvas id='300_South_Riverside_Plaza' height='300'></canvas>";
 
 let divChapter3 =
   "<h3>311 South Wacker</h3>" +
   // "<p><strong>Status:</strong> For sale, since Sept. 2021<br><strong>Owner:</strong> JV of Zeller Realty and Cindat<br><strong>Current first mortgage:</strong> $215M, Morgan Stanley, 2018<br><strong>Mezzanine loan:</strong> Approx. $90M, Nuveen, 2018<br><strong>Occupancy:</strong> 61% leased<br><strong>Last sale price:</strong> $302M, 2014 ($230 psf) plus $38M spent on renovations<br><strong>Year built:</strong> 1990</p>" +
-  "<canvas id='311_South_Wacker' height='400'></canvas>";
+  "<canvas id='311_South_Wacker' height='300'></canvas>";
 
 let divChapter4 = 
   "<h3>216 West Jackson</h3>" +
   // "<p><strong>Status:</strong> In receivership<br><strong>Owner:</strong> Marc Realty<br><strong>Current mortgage:</strong> $16.5M, CMBS debt originated by German American Capital Corp., 2013<br><strong>Occupancy:</strong> 26% leased<br><strong>Last sale price:</strong> $22.3M, 2013 ($112 psf)<br><strong>Latest appraisal:</strong> $7M, July 2023<br><strong>Year built:</strong> 1897</p>" +
-  "<canvas id='216_West_Jackson' height='400'></canvas>";
+  "<canvas id='216_West_Jackson' height='300'></canvas>";
 
 let divChapter5 = 
   "<h3>209 West Jackson</h3>" +
   // "<p><strong>Status: </strong>Lender-owned via deed in lieu of foreclosure, June 2023<br><strong>Owner: </strong>ACRES Capital, following surrender by Market Street Partners<br><strong>Last mortgage: </strong>$25M CMBS debt originated by ACRES Capital<br><strong>Last sale price: </strong>$23.3M, 2018 ($162 psf)<br><strong>Year built: </strong>1896" +
-   "<canvas id='209_West_Jackson' height='400'></canvas>";
+   "<canvas id='209_West_Jackson' height='300'></canvas>";
 
 let divChapter6 =
   "<h3>175 West Jackson</h3>" +
   // "<p><strong>Status: </strong>In receivership, for sale<br><strong>Owner: </strong>Brookfield Asset Management<br><strong>Last mortgage: </strong>$280M CMBS debt originated by German American Capital Corp., 2013<br><strong>Appraised value at loan issuance: </strong>$410M, 2013 ($292 psf)<br><strong>Last sale price: </strong>$306M, 2018 ($220 psf)<br><strong>Occupancy: </strong>59% leased<br><strong>Latest appraisal: </strong>$195M, 2022 ($139 psf)<br><strong>Year built: </strong>1912</p>" +
-  "<canvas id='175_West_Jackson' height='400'></canvas>";
+  "<canvas id='175_West_Jackson' height='300'></canvas>";
 
 let divChapter7 =
   "<h3>141 West Jackson (Board of Trade building)</h3>" +
 //  "<p><strong>Status: </strong>Lender-owned via deed in lieu of foreclosure<br><strong>Owner: </strong>Apollo Global Management, following surrender by JV of Glenstar and Oaktree Capital Management<br><strong>Last senior mortgage: </strong>$198M, Apollo Global, 2020 ($168M balance at time of January transfer)<br><strong>Mezzanine debt: </strong>$58M, CIM Group, 2020 ($49M balance at time of January transfer)<br><strong>Occupancy: </strong>85% leased<br><strong>Last sale price: </strong>$152M, 2012 ($109 psf)<br><strong>Year built: </strong>1930" +
-  "<canvas id='141_West_Jackson' height='400'></canvas>";
+  "<canvas id='141_West_Jackson' height='300'></canvas>";
 
 let divChapter8 =
   "<h3>111 West Jackson</h3>" +
   // "<p><strong>Status: </strong>Borrower in danger of imminent default<br><strong>Owner: </strong>Melohn Group<br><strong>Current mortgage: </strong>$105M CMBS debt originated by Nataxis, 2017, matures Dec. 2027<br><strong>Occupancy: </strong>65% leased<br><strong>Last sale price: </strong>$135M, 2013 ($235 psf)<br><strong>Year built: </strong>1962" +
-  "<canvas id='111_West_Jackson' height='400'></canvas>";
+  "<canvas id='111_West_Jackson' height='300'></canvas>";
 
 let divChapter9 = 
   "<h3>55 East Jackson</h3>" +
   // "<p><strong>Status: </strong>Leasehold owner in default of ground lease<br><strong>Leasehold owner: </strong>Firenze Group<br><strong>Current leasehold mortgage: </strong>$44M CMBS debt originated by Ready Capital, 2019<br><strong>Ground lease debt: </strong>None<br><strong>Ground landlord: </strong>Marc Realty<br><strong>Occupancy: </strong>47% leased<br><strong>Last sale price:</strong> $64M for leasehold only, 2019<br><strong>Year built: </strong>1961</p>" +
-  "<canvas id='55_East_Jackson' height='400'></canvas>";
+  "<canvas id='55_East_Jackson' height='300'></canvas>";
 
   function createChart() {
     var ctx = document.getElementById('600_West_Jackson').getContext('2d');
