6e6655bab38d01487e208bcf3dc798a778da11d1
AFarence <83982643+AFarence@users.noreply.github.com>
2023-07-31 18:46:31 -0400

    Update map.js

--- Boulevard_broken_deals/map.js
+++ Boulevard_broken_deals/map.js
@@ -269,7 +269,7 @@ map.on("load", function () {
         center: chapter.location.center,
         pitch: chapter.location.pitch,
         zoom: thisZoom,
-        speed: 0.2, // Adjust this value as needed to change the transition speed
+        speed: 0.3, // Adjust this value as needed to change the transition speed
         curve: 1 // Optional, this value would affect the flight speed curve
       };
       map[chapter.mapAnimation || "flyTo"](thisLocation);
