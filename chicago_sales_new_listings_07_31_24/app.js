8a24db921cff66e065689367674e4f1ccd8f6738
AFarence <83982643+AFarence@users.noreply.github.com>
2024-08-02 17:24:10 -0400

    Chicago Map

--- chicago_sales_new_listings_07_31_24/app.js
+++ chicago_sales_new_listings_07_31_24/app.js
@@ -13,7 +13,7 @@ const bounds = [
 document.addEventListener('DOMContentLoaded', function() {
     var map = new mapboxgl.Map({
         container: 'map',
-        style: "mapbox://styles/trddata/cl6h00632006t14o08g2ppqvj",
+        style: "mapbox://styles/trddata/clrax4la3005701qogu72fl71",
         center: centerChicago,
         zoom: 9,
         maxBounds: bounds
@@ -40,28 +40,29 @@ document.addEventListener('DOMContentLoaded', function() {
     });
     
     map.on('load', function() {
-        // Load the GeoJSON data directly
-        fetch('output.geojson').then(response => response.json()).then(geojsonData => {
-            // Assuming you want to add the loaded GeoJSON data to the map as a source
-            map.addSource('conversions', {
+        // Load the first GeoJSON data directly
+        fetch('output_sales_new_listings.geojson').then(response => response.json()).then(geojsonData => {
+            // Add the first GeoJSON data as a source
+            map.addSource('sales_new_listings', {
                 type: 'geojson',
                 data: geojsonData
             });
     
-            // Then add a layer to display this data
+            // Add a layer to display the first GeoJSON data
             map.addLayer({
-                id: 'count-layer',
+                id: 'sales_new_listings_layer',
                 type: 'circle',
-                source: 'conversions',
+                source: 'sales_new_listings',
                 paint: {
                     'circle-radius': 10,
                     'circle-color': [
                         'match',
-                        ['get', 'color'], // Assumes 'type' is the property name in your GeoJSON
+                        ['get', 'color'], // Assumes 'color' is the property name in your GeoJSON
                         'yellow', '#FFFF00',
                         'red', '#FF0000',
-                        'blue', '#0000FF',
-                        'green','#00FF00',
+                        'cyan', '#00FFFF',
+                        'green', '#00FF00',
+                        'white', '#FFFFFF',
                         '#000000'
                     ],
                     'circle-stroke-color': '#000000',
@@ -69,22 +70,75 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             });
         });
-    // Show the legend
-    document.getElementById('legend').style.display = 'block';
+    
+        // Load the second GeoJSON data directly
+        fetch('output_foreclosures_refinances.geojson').then(response => response.json()).then(geojsonData2 => {
+            // Add the second GeoJSON data as a source
+            map.addSource('foreclosures_refinances', {
+                type: 'geojson',
+                data: geojsonData2
+            });
+    
+            // Add a layer to display the second GeoJSON data
+            map.addLayer({
+                id: 'foreclosures_refinances_layer',
+                type: 'circle',
+                source: 'foreclosures_refinances',
+                paint: {
+                    'circle-radius': 10,
+                    'circle-color': [
+                        'match',
+                        ['get', 'type'], // Assumes 'type' is the property name in your GeoJSON
+                        'Refinance', '#FFFF00', // Yellow for Refinance
+                        'Foreclosure', '#FF0000', // Red for Foreclosure
+                        '#000000' // Default color if neither match
+                    ],
+                    'circle-stroke-color': '#000000',
+                    'circle-stroke-width': 1
+                }
+            });
+
+        // Show the legend
+        document.getElementById('legend').style.display = 'block';
+    });
+    
 
     });
     
-        const excludeFields = ['full_address', 'geocoded','lat','lon','type','color','story_link']; // Add your field names to this array
+        const excludeFields = ['full_address', 'geocoded','lat','lon','type','color','story_link','loan_int','loan_sh','type']; // Add your field names to this array
 
-        map.on('click', 'count-layer', (e) => {
+        map.on('click', 'sales_new_listings_layer', (e) => {
             const properties = e.features[0].properties;
             let popupContent = '<div class="popup-content"><h3>Details</h3>';
             
             for (const key in properties) {
                 if (properties[key] !== 'nan' && !excludeFields.includes(key)) {
-                    popupContent += `<div class="popup-field"><span class="popup-key">${key}:</span> <span class="popup-value">${properties[key]}</span></div>`;
+                    // Convert key to title case
+                    const titleCaseKey = key.replace(/\b\w/g, char => char.toUpperCase());
+                    popupContent += `<div class="popup-field"><span class="popup-key">${titleCaseKey}:</span> <span class="popup-value">${properties[key]}</span></div>`;
                 }
             }
+            
+            popupContent += '</div>';
+    
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(popupContent)
+                .addTo(map);
+        });
+
+        map.on('click', 'foreclosures_refinances_layer', (e) => {
+            const properties = e.features[0].properties;
+            let popupContent = '<div class="popup-content"><h3>Details</h3>';
+            
+            for (const key in properties) {
+                if (properties[key] !== 'nan' && !excludeFields.includes(key)) {
+                    // Convert key to title case
+                    const titleCaseKey = key.replace(/\b\w/g, char => char.toUpperCase());
+                    popupContent += `<div class="popup-field"><span class="popup-key">${titleCaseKey}:</span> <span class="popup-value">${properties[key]}</span></div>`;
+                }
+            }
+            
             popupContent += '</div>';
     
             new mapboxgl.Popup()
@@ -94,13 +148,24 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     
         // Enhance interactivity on hover
-        map.on('mouseenter', 'count-layer', function() {
+        map.on('mouseenter', 'sales_new_listings_layer', function() {
             map.getCanvas().style.cursor = 'pointer';
-            map.setPaintProperty('count-layer', 'circle-stroke-width', 3); // Thicker stroke on hover
+            map.setPaintProperty('sales_new_listings_layer', 'circle-stroke-width', 3); // Thicker stroke on hover
         });
     
-        map.on('mouseleave', 'count-layer', function() {
+        map.on('mouseleave', 'sales_new_listings_layer', function() {
             map.getCanvas().style.cursor = '';
-            map.setPaintProperty('count-layer', 'circle-stroke-width', 1); // Reset stroke width
+            map.setPaintProperty('sales_new_listings_layer', 'circle-stroke-width', 1); // Reset stroke width
         });
+
+        map.on('mouseenter', 'foreclosures_refinances_layer', function() {
+            map.getCanvas().style.cursor = 'pointer';
+            map.setPaintProperty('foreclosures_refinances_layer', 'circle-stroke-width', 3); // Thicker stroke on hover
+        });
+    
+        map.on('mouseleave', 'foreclosures_refinances_layer', function() {
+            map.getCanvas().style.cursor = '';
+            map.setPaintProperty('foreclosures_refinances_layer', 'circle-stroke-width', 1); // Reset stroke width
+        });
+
     });
