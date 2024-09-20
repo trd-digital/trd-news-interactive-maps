0426a904eb5e0bf3c05818aa164be8d2868e762e
AFarence <83982643+AFarence@users.noreply.github.com>
2024-09-20 10:50:51 -0400

    Brandon Miller map

---
+++ NYC_Brandon_Miller_09_20_24/app.js
@@ -0,0 +1,108 @@
+// Your Mapbox access token
+mapboxgl.accessToken = "pk.eyJ1IjoidHJkZGF0YSIsImEiOiJjamc2bTc2YmUxY2F3MnZxZGh2amR2MTY5In0.QlOWqB-yQNrNlXD0KQ9IvQ"; 
+
+const centerNYC = [-74.0060, 40.7128]; // NYC's longitude and latitude
+const longitudeSpan = 1.5;
+const latitudeSpan = 0.75;
+
+const bounds = [
+    [centerNYC[0] - longitudeSpan, centerNYC[1] - latitudeSpan], // [westLongitude, southLatitude]
+    [centerNYC[0] + longitudeSpan, centerNYC[1] + latitudeSpan]  // [eastLongitude, northLatitude]
+];
+
+document.addEventListener('DOMContentLoaded', function() {
+    var map = new mapboxgl.Map({
+        container: 'map',
+        style: "mapbox://styles/trddata/clrax4la3005701qogu72fl71",
+        center: centerNYC,
+        zoom: 9,
+        maxBounds: bounds
+    });
+
+    // Add zoom and rotation controls to the map.
+    map.addControl(new mapboxgl.NavigationControl());
+
+    // Position the info box
+
+    map.on('load', function() {
+        // Load the GeoJSON data
+        fetch('out_put_brandon_miller_properties.geojson')
+            .then(response => response.json())
+            .then(geojsonData => {
+                // Add the GeoJSON data as a source
+                map.addSource('properties', {
+                    type: 'geojson',
+                    data: geojsonData
+                });
+
+                // Add a layer to display the GeoJSON data
+                map.addLayer({
+                    id: 'properties_layer',
+                    type: 'circle',
+                    source: 'properties',
+                    paint: {
+                        'circle-radius': 10,
+                        'circle-color': '#FFFFFF',
+                        'circle-stroke-color': '#000000',
+                        'circle-stroke-width': 1
+                    }
+                });
+            })
+            .catch(error => {
+                console.error('Error loading GeoJSON data:', error);
+            });
+
+        // Disable the default scroll zoom behavior
+        map.scrollZoom.disable();
+
+        // Function to handle enabling/disabling scroll zoom
+        function handleScrollZoom(event) {
+            if (event.ctrlKey || event.metaKey) {
+                map.scrollZoom.enable();
+            } else {
+                map.scrollZoom.disable();
+            }
+        }
+
+        // Add event listeners for scroll zoom control
+        window.addEventListener('keydown', handleScrollZoom);
+        window.addEventListener('keyup', handleScrollZoom);
+
+        // Fields to exclude from the popup
+        const excludeFields = ['Nabe', 'Acris link', 'LLC', 'State court',
+            'Federal court', 'Clips', 'Floors', 'SF', 'Units', 'Unnamed: 11',
+            'full_address', 'geocoded', 'lat', 'lon', 'geometry'];
+
+        // Click event handler for displaying popups
+        map.on('click', 'properties_layer', (e) => {
+            const properties = e.features[0].properties;
+            let popupContent = '<div class="popup-content"><h3>Details</h3>';
+            
+            for (const key in properties) {
+                if (properties[key] !== 'nan' && !excludeFields.includes(key)) {
+                    // Convert key to title case
+                    const titleCaseKey = key.replace(/\b\w/g, char => char.toUpperCase());
+                    popupContent += `<div class="popup-field"><span class="popup-key">${titleCaseKey}:</span> <span class="popup-value">${properties[key]}</span></div>`;
+                }
+            }
+            
+            popupContent += '</div>';
+
+            new mapboxgl.Popup()
+                .setLngLat(e.lngLat)
+                .setHTML(popupContent)
+                .addTo(map);
+        });
+
+        // Enhance interactivity on hover
+        map.on('mouseenter', 'properties_layer', function() {
+            map.getCanvas().style.cursor = 'pointer';
+            map.setPaintProperty('properties_layer', 'circle-stroke-width', 3); // Thicker stroke on hover
+        });
+
+        map.on('mouseleave', 'properties_layer', function() {
+            map.getCanvas().style.cursor = '';
+            map.setPaintProperty('properties_layer', 'circle-stroke-width', 1); // Reset stroke width
+        });
+    });
+});
