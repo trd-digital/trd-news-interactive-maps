15d918d6650fcda1fec536389f5a0ce128729109
AFarence <83982643+AFarence@users.noreply.github.com>
2024-09-25 11:07:41 -0400

    Updated Brandon Miller Map
    Added in cluster marking
    Added in missing properties
    Fixed zoom.

--- NYC_Brandon_Miller_09_20_24/app.js
+++ NYC_Brandon_Miller_09_20_24/app.js
@@ -5,6 +5,9 @@ const centerNYC = [-74.0060, 40.7128]; // NYC's longitude and latitude
 const longitudeSpan = 1.5;
 const latitudeSpan = 0.75;
 
+excludeFields = ['Neighborhood', 'State', 'full_address',
+       'geocoded', 'lat', 'lon', 'geometry']
+
 const bounds = [
     [centerNYC[0] - longitudeSpan, centerNYC[1] - latitudeSpan], // [westLongitude, southLatitude]
     [centerNYC[0] + longitudeSpan, centerNYC[1] + latitudeSpan]  // [eastLongitude, northLatitude]
@@ -13,11 +16,15 @@ const bounds = [
 document.addEventListener('DOMContentLoaded', function() {
     var map = new mapboxgl.Map({
         container: 'map',
-        style: "mapbox://styles/trddata/clrax4la3005701qogu72fl71",
+        style: "mapbox://styles/trddata/ck0e4y50i14as1cnx6qvtbxi6",
         center: centerNYC,
-        zoom: 10,
-        maxBounds: bounds
+        zoom: 3,
+        minZoom: 3,
+        // maxBounds: bounds
     });
+
+    // Force map to fit the entire world at the lowest possible zoom
+    // map.fitBounds([[-90, -43], [90, 43]]);
 
     // Add zoom and rotation controls to the map.
     map.addControl(new mapboxgl.NavigationControl());
@@ -29,90 +36,130 @@ document.addEventListener('DOMContentLoaded', function() {
         fetch('data.geojson')
             .then(response => response.json())
             .then(geojsonData => {
-                // Add the GeoJSON data as a source
+                // Add the GeoJSON data as a source with clustering enabled
                 map.addSource('properties', {
                     type: 'geojson',
-                    data: geojsonData
+                    data: geojsonData,
+                    cluster: true,               // Enable clustering
+                    clusterMaxZoom: 14,           // Max zoom to cluster points
+                    clusterRadius: 50             // Radius of each cluster when clustering points (adjust as needed)
                 });
-
-                // Add a layer to display the GeoJSON data
+    
+                // Add a layer for clustered circles
                 map.addLayer({
-                    id: 'properties_layer',
+                    id: 'clusters',
                     type: 'circle',
                     source: 'properties',
+                    filter: ['has', 'point_count'],  // Only show cluster points
                     paint: {
-                        'circle-radius': 10,
-                        'circle-color': '#FFFFFF',
-                        'circle-stroke-color': '#000000',
-                        'circle-stroke-width': 1
+                        'circle-color': [
+                            'step',
+                            ['get', 'point_count'],
+                            '#51bbd6',   // Color for small clusters
+                            100, '#f1f075', // Color for medium clusters
+                            750, '#f28cb1'  // Color for large clusters
+                        ],
+                        'circle-radius': [
+                            'step',
+                            ['get', 'point_count'],
+                            15, 100, 20, 750, 25  // Adjust sizes based on cluster size
+                        ]
                     }
+                });
+    
+                // Add a layer for the cluster count labels
+                map.addLayer({
+                    id: 'cluster-count',
+                    type: 'symbol',
+                    source: 'properties',
+                    filter: ['has', 'point_count'],  // Only show counts on clusters
+                    layout: {
+                        'text-field': '{point_count_abbreviated}',  // Display cluster count
+                        'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
+                        'text-size': 12
+                    }
+                });
+    
+                // Add a layer for unclustered points
+                map.addLayer({
+                    id: 'unclustered-point',
+                    type: 'circle',
+                    source: 'properties',
+                    filter: ['!', ['has', 'point_count']],  // Show only non-clustered points
+                    paint: {
+                        'circle-color': '#11b4da',
+                        'circle-radius': 10,
+                        'circle-stroke-width': 1,
+                        'circle-stroke-color': '#fff'
+                    }
+                });
+    
+                // Event handler for popups on unclustered points
+                map.on('click', 'unclustered-point', (e) => {
+                    const properties = e.features[0].properties;
+    
+                    // Replace 'Details' with the address if available
+                    const address = properties['Address'] || 'Details';  // Use 'Details' as fallback if no address
+    
+                    let popupContent = `<div class="popup-content"><h3>${address}</h3>`;
+    
+                    for (const key in properties) {
+                        if (properties[key] !== 'nan' && !excludeFields.includes(key)) {
+                            const titleCaseKey = key.replace(/\b\w/g, char => char.toUpperCase());
+                            if (key === 'Description' && properties[key].includes('<a href=')) {
+                                popupContent += `<div class="popup-field"><span class="popup-key"></span> ${properties[key]}</div>`;
+                            } else {
+                                popupContent += `<div class="popup-field"><span class="popup-key"></span> <span class="popup-value">${properties[key]}</span></div>`;
+                            }
+                        }
+                    }
+    
+                    popupContent += '</div>';
+    
+                    new mapboxgl.Popup()
+                        .setLngLat(e.lngLat)
+                        .setHTML(popupContent)  // This will render the HTML
+                        .addTo(map);
+                });
+    
+                // Zoom in when clicking on clusters
+                map.on('click', 'clusters', (e) => {
+                    const features = map.queryRenderedFeatures(e.point, {
+                        layers: ['clusters']
+                    });
+                    const clusterId = features[0].properties.cluster_id;
+                    map.getSource('properties').getClusterExpansionZoom(
+                        clusterId,
+                        (err, zoom) => {
+                            if (err) return;
+    
+                            map.easeTo({
+                                center: features[0].geometry.coordinates,
+                                zoom: zoom
+                            });
+                        }
+                    );
+                });
+    
+                // Enhance interactivity on hover
+                map.on('mouseenter', 'unclustered-point', function() {
+                    map.getCanvas().style.cursor = 'pointer';
+                });
+    
+                map.on('mouseleave', 'unclustered-point', function() {
+                    map.getCanvas().style.cursor = '';
+                });
+    
+                map.on('mouseenter', 'clusters', function() {
+                    map.getCanvas().style.cursor = 'pointer';
+                });
+    
+                map.on('mouseleave', 'clusters', function() {
+                    map.getCanvas().style.cursor = '';
                 });
             })
             .catch(error => {
                 console.error('Error loading GeoJSON data:', error);
             });
-
-        // Disable the default scroll zoom behavior
-        map.scrollZoom.disable();
-
-        // Function to handle enabling/disabling scroll zoom
-        function handleScrollZoom(event) {
-            if (event.ctrlKey || event.metaKey) {
-                map.scrollZoom.enable();
-            } else {
-                map.scrollZoom.disable();
-            }
-        }
-
-        // Add event listeners for scroll zoom control
-        window.addEventListener('keydown', handleScrollZoom);
-        window.addEventListener('keyup', handleScrollZoom);
-
-        // Fields to exclude from the popup
-        const excludeFields = ['Address','Neighborhood', 'full_address', 'geocoded',
-       'lat', 'lon', 'geometry'];
-
-        // Click event handler for displaying popups
-        map.on('click', 'properties_layer', (e) => {
-            const properties = e.features[0].properties;
-            
-            // Replace 'Details' with the address if available
-            const address = properties['Address'] || 'Details';  // Use 'Details' as fallback if no address
-            
-            let popupContent = `<div class="popup-content"><h3>${address}</h3>`;  // Dynamic heading
-            
-            for (const key in properties) {
-                if (properties[key] !== 'nan' && !excludeFields.includes(key)) {
-                    // Convert key to title case
-                    const titleCaseKey = key.replace(/\b\w/g, char => char.toUpperCase());
-        
-                    // Check if this is the Description key and has a hyperlink
-                    if (key === 'Description' && properties[key].includes('<a href=')) {
-                        popupContent += `<div class="popup-field"><span class="popup-key"></span> ${properties[key]}</div>`;
-                    } else {
-                        popupContent += `<div class="popup-field"><span class="popup-key"></span> <span class="popup-value">${properties[key]}</span></div>`;
-                    }
-                }
-            }
-        
-            popupContent += '</div>';
-        
-            new mapboxgl.Popup()
-                .setLngLat(e.lngLat)
-                .setHTML(popupContent)  // This will render the HTML
-                .addTo(map);
-        });        
-        
-
-        // Enhance interactivity on hover
-        map.on('mouseenter', 'properties_layer', function() {
-            map.getCanvas().style.cursor = 'pointer';
-            map.setPaintProperty('properties_layer', 'circle-stroke-width', 3); // Thicker stroke on hover
-        });
-
-        map.on('mouseleave', 'properties_layer', function() {
-            map.getCanvas().style.cursor = '';
-            map.setPaintProperty('properties_layer', 'circle-stroke-width', 1); // Reset stroke width
-        });
-    });
+    });    
 });
