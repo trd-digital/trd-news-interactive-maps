2c5b77655c86427f3f03eb738f35cf33d4421343
Joseph Jungermann <joseph.jungermann@journalism.cuny.edu>
2024-10-22 12:43:26 -0400

    [TRD-3728] create map

---
+++ MD_Agents_Map_20241009/data/top_transactions_sample.js
@@ -0,0 +1,35 @@
+var transactions1 = {
+  "type": "FeatureCollection",
+	"features": [
+  {
+    "MLS_Number": "A11392365",
+    "Address": "12 Indian Creek Island Rd",
+    "Location": "INDIAN CREEK GOLF CLUB IS",
+    "Price": "$79,000,000",
+    "Final_Agent_Team": "DINA GOLDENTAYER",
+    "Final_Brokerage": "Douglas Elliman",
+    "Lat": 25.87815979,
+    "Lon": -80.14161024
+  },
+  {
+    "MLS_Number": "A11450250",
+    "Address": "11 Indian Creek Island Rd",
+    "Location": "INDIAN CREEK GOLF CLUB IS",
+    "Price": "$68,000,000",
+    "Final_Agent_Team": "GABRIELA ESPEJO",
+    "Final_Brokerage": "Golden Ocean Real Estate Corp",
+    "Lat": 25.87886739,
+    "Lon": -80.14170226
+  },
+  {
+    "MLS_Number": "A11434781",
+    "Address": "27 Star Island Dr",
+    "Location": "CORRECTED PL OF STAR ISLA",
+    "Price": "$57,000,000",
+    "Final_Agent_Team": "THE CARROLL GROUP",
+    "Final_Brokerage": "Compass Florida, LLC",
+    "Lat": 25.78039022,
+    "Lon": -80.15007201
+  }
+]
+};
---
+++ MD_Agents_Map_20241009/map.js
@@ -0,0 +1,112 @@
+// SETUP MAP
+// Initialize map, set the view lat long and zoom 
+var map = L.map('map').setView([25.7939, -80.18825], 10.6);
+
+// Import map style tile layer from MapBox with api key
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, Imagery &copy; <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/dark-v9',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoiam9zZXBoanVuZ2VybWFubiIsImEiOiJjazl5b3lldHUwMGF1M21wYWZxZGxtaXFrIn0.SIGcJf1ElkmPBC0Gs31ktw',
+    scrollWheelZoom: false
+}).addTo(map);
+
+//SETUP AND CALL JS VARIABLES FOR DISPLAY IN POP-UP MENU
+// Loop through the js data point to create markers for each transaction. 
+// Variable from js storing all data including lat and lon is named "transactions"
+transactions.features.forEach(function(feature) {
+    // Get the coordinates and property details
+    var lat = feature.Latitude;
+    var lon = feature.Longitude;
+    var address = feature.Address;
+    var price = feature["Sale Price"];
+    var listAgentOne = feature["Listing Agent/Team 1"];
+    var listBrokerageOne = feature["Listing Brokerage 1"];
+    var listAgentTwo = feature["Listing Agent/Team 2"];
+    var listBrokerageTwo = feature["Listing Brokerage 2"];
+    var buyAgentOne = feature["Buying Agent/Team 1"];
+    var buyBrokerageOne = feature["Buying Brokerage 1"];
+    var buyAgentTwo = feature["Buying Agent/Team 2"];
+    var buyBrokerageTwo = feature["Buying Brokerage 2"];
+    var mls = feature["MLS Number"]; 
+
+//SETUP CIRCLE MARKERS THAT MARK EACH POINT AND SIZE BASE ON PRICE
+// Function to convert price to a radius size
+function getRadius(price) {
+    // Remove the dollar sign and commas, then convert to a number
+    var numericPrice = parseFloat(price.replace(/[^0-9.-]+/g, ''));
+    // Adjust the scale factor to your preference for the marker size
+    var scaleFactor = 0.002;
+    return Math.sqrt(numericPrice) * scaleFactor;
+}
+
+    // Create a circle marker for each location with size based on price
+    var circleMarker = L.circleMarker([lat, lon], {
+        radius: getRadius(price),
+        color: '#ffffff',
+        weight: 1,
+        fillColor: '#0000ff',
+        fillOpacity: 0.5
+    }).addTo(map);
+ 
+//ADD INFORMATION TO POP-UP AND DISPLAY ON EACH MARKER
+    //This pop-up is dynamic. 
+    //If there is no value in a category, i.e. Listing Agent/Team 2 is blank, the category will not display for the pop-up
+    var popupContent = `<h3>Transaction Details</h3>`;
+
+    if (address) popupContent += `<b>Address:</b> ${address}<br>`;
+    if (price) popupContent += `<b>Price:</b> ${price}<br>`;
+    if (listAgentOne) popupContent += `<b>Listing Agent/Team 1:</b> ${listAgentOne}<br>`;
+    if (listBrokerageOne) popupContent += `<b>Listing Brokerage 1:</b> ${listBrokerageOne}<br>`;
+    if (listAgentTwo) popupContent += `<b>Listing Agent/Team 2:</b> ${listAgentTwo}<br>`;
+    if (listBrokerageTwo) popupContent += `<b>Listing Brokerage 2:</b> ${listBrokerageTwo}<br>`;
+    if (buyAgentOne) popupContent += `<b>Buying Agent/Team 1:</b> ${buyAgentOne}<br>`;
+    if (buyBrokerageOne) popupContent += `<b>Buying Brokerage 1:</b> ${buyBrokerageOne}<br>`;
+    if (buyAgentTwo) popupContent += `<b>Buying Agent/Team 2:</b> ${buyAgentTwo}<br>`;
+    if (buyBrokerageTwo) popupContent += `<b>Buying Brokerage 2:</b> ${buyBrokerageTwo}<br>`;
+    if (mls) popupContent += `<b>MLS Number:</b> ${mls}<br>`;
+
+// Add the dynamically generated popup to the circle marker
+circleMarker.bindPopup(popupContent);
+});
+
+//BELOW IS OLD CODE THAT WOULD INCLUDE ALL CATEGORICAL INFORMATION EVEN IF THE VALUE WAS BLANK
+//     // Add a popup to the circle marker with property information
+//     circleMarker.bindPopup(
+//         `<h3>Transaction Details</h3>
+//         <b>Address:</b> ${address}<br>
+//          <b>Price:</b> ${price}<br>
+//          <b>Listing Agent/Team 1:</b> ${listAgentOne}<br>
+//          <b>Listing Brokerage 1:</b> ${listBrokerageOne}<br>
+//          <b>Listing Agent/Team 2:</b> ${listAgentTwo}<br>
+//          <b>Listing Brokerage 2:</b> ${listBrokerageTwo}<br>
+//          <b>Buying Agent/Team 1:</b> ${buyAgentOne}<br>
+//          <b>Buying Brokerage 1:</b> ${buyBrokerageOne}<br>
+//          <b>Buying Agent/Team 2:</b> ${buyAgentTwo}<br>
+//          <b>Buying Brokerage 2:</b> ${buyBrokerageTwo}<br>
+//          <b>MLS Number:</b> ${mls}<br>`
+
+
+//BELOW CODE IS IF WE WANT TO USE PIN MARKERS INSTEAD OF VARIABLE SIZING CIRCLE MARKERS
+//     // Use this block of code to create pin markers for each location instead of sizeable circles
+//     var marker = L.marker([lat, lon]).addTo(map);
+
+//     // Add a popup to the marker with property information
+//     marker.bindPopup(
+//         `<h3>Transaction Details</h3>
+//         <b>Address:</b> ${address}<br>
+//          <b>Price:</b> ${price}<br>
+//          <b>Listing Agent/Team 1:</b> ${listAgentOne}<br>
+//          <b>Listing Brokerage 1:</b> ${listBrokerageOne}<br>
+//          <b>Listing Agent/Team 2:</b> ${listAgentTwo}<br>
+//          <b>Listing Brokerage 2:</b> ${listBrokerageTwo}<br>
+//          <b>Buying Agent/Team 1:</b> ${buyAgentOne}<br>
+//          <b>Buying Brokerage 1:</b> ${buyBrokerageOne}<br>
+//          <b>Buying Agent/Team 2:</b> ${buyAgentTwo}<br>
+//          <b>Buying Brokerage 2:</b> ${buyBrokerageTwo}<br>
+//          <b>MLS Number:</b> ${mls}<br>`
+//     );
+// });
+
